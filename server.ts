b6bd777ca211f06fd807a4911e4701dc93df378f
ranasheikh64 <rofikul6424islam@gmail.com>
2026-06-15 18:13:30 +0600

    Fix import extension to .js for ESM compatibility

--- server.ts
+++ server.ts
@@ -8,8 +8,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+dotenv.config();
 
 const app = express();
 const PORT = 3000;
